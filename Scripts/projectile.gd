dfdc993280b760f98dff6e2b64813729b883faf1
chaydata <chris.r.j.hay@gmail.com>
2025-02-23 00:43:27 -0600

    2player controls, fixed banana direction, fixed 2P

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -3,11 +3,14 @@ extends Area2D
 @onready var _animation_player = $AnimationPlayer
 
 var speed = 300
-var direction = 1
+var direction = 1 # Default, but will be set by the player
+
+func _ready():
+	scale.x = direction  # Flip the sprite if moving left (this solves frozen banana issue)
 
 func _physics_process(delta):
 	position.x += speed * direction * delta
 	_animation_player.play("throw")
 	
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	queue_free()
