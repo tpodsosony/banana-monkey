c4d2662c4709beb6b5ebdd3fe526eb92a7dde569
chaydata <chris.r.j.hay@gmail.com>
2025-02-23 01:47:16 -0600

    Added controls & credits to start menu
    Extra stuff we probably didn't need but was fun. Controls are important I guess.

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -3,8 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	# Initially hide the controls
+	$Controls.hide()
+	$Credits.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,6 +17,20 @@ func _on_start_pressed() -> void:
 	print("Start pressed")
 	get_tree().change_scene_to_file("res://Scenes/FightingScene.tscn")
 
+# This function is connected to the toggle button
+func _on_controls_toggled(toggled_on: bool) -> void:
+	# Show or hide controls based on toggle state
+	if toggled_on:
+		$Controls.visible = true
+	else:
+		$Controls.visible = false
+
+func _on_credits_toggled(toggled_on: bool) -> void:
+	# Show or hide credits based on toggle state
+	if toggled_on:
+		$Credits.visible = true
+	else:
+		$Credits.visible = false
 
 func _on_exit_pressed() -> void:
 	# prints "Exit pressed" to debugger screen thingie =)
