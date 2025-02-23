8616472353aba129ef1fd9f54b7bf0b8d665dda5
Chris Hay <chris.r.j.hay@gmail.com>
2025-02-22 18:16:33 -0600

    health bar stuff

--- Scripts/p1_health_bar.gd
+++ Scripts/p1_health_bar.gd
@@ -1,16 +1,17 @@
 ## PSA: Ctrl + K to comment out, ctrl + / to go back
-extends ProgressBar
+extends TextureProgressBar
 
 
 ## reference the player 1 variable (we use export to be able to get it here)
 @export var player_1: player_1
+# reference value from TextureProgressBar
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var root_node = get_parent()
 	player_1.connect("healthChanged", update)
-	update()	
+	update()
 
 func update():
 	value = player_1.currentHealth * 100 / player_1.maxHealth
